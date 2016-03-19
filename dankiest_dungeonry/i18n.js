e9703ad94ad41212756c185a3c93c77edc1cd5f4
JavaMatrix <defensoft@gmail.com>
2016-03-19 17:02:24 -0400

    quick test on i18n

---
+++ dankiest_dungeonry/i18n.js
@@ -0,0 +1,16 @@
+var I18n = {};
+
+I18n.Load = function()
+{
+    $.get("./data/i18n.yaml", I18n.LoadFile);
+}
+
+I18n.LoadFile = function(text)
+{
+    var doc = yaml.safeLoad(text);
+    for (var tag in doc) {
+        console.log(tag);
+    }
+}
+
+I18n.Load();
